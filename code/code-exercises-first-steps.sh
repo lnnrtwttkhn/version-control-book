d13b3419106e9be6c8b2a905c48124e50fc39831
Lennart Wittkuhn <lennart.wittkuhn@tutanota.com>
2024-07-16 09:35:46 +0200

    Add shell script to implement exercises for first steps with Git chapter

---
+++ code/code-exercises-first-steps.sh
@@ -0,0 +1,27 @@
+# Initialize a Git repository:
+mkdir recipes # <1>
+cd recipes # <2>
+git init # <3>
+# Add content and commit changes:
+touch recipes.txt # <4>
+cat > recipes.txt <<- EOM
+Pasta
+
+1. Bring water to the boil in a large saucepan.
+2. Once the water is boiling, add a good pinch of salt.
+3. Add the pasta.
+4. Cook the pasta according to packet instructions until it's "al dente".
+5. Drain the pasta in a colander.
+EOM
+# <5>
+git add recipes.txt # <6>
+git commit -m "Add pasta recipe to recipes.txt" # <7>
+# Amend a commit:
+echo "6. Enjoy!" >> recipes.txt # <8>
+git add recipes.txt # <9>
+git commit --amend --no-edit # <10>
+# Create a .gitignore file:
+wget  -O pasta.jpg https://upload.wikimedia.org/wikipedia/commons/8/84/Rigatoni.jpg # <10>
+echo "*.jpg" > .gitignore # <11>
+git add --all # <12>
+git commit -m "Add .gitignore and ignore all files that end with .jpg" # <13>
