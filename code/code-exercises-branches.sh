22a4a78ff162d82bb7ef0364086726ba3fc46d2b
konradpa <konrad@pagenstedt.de>
2024-07-22 12:34:21 +0200

    add solutions to branches exercises

--- code/code-exercises-branches.sh
+++ code/code-exercises-branches.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Navigate to the existing 'recipes' directory
-cd recipes
+cd recipes # <1>
 
 # Create a new branch called 'feature'
-git checkout -b feature
+git checkout -b feature # <2>
 
 # Add a new recipe to your recipes.txt file
-echo "Chocolate Cake" >> recipes.txt
+echo "Chocolate Cake" >> recipes.txt # <3>
 cat >> recipes.txt <<- EOM
 
 1. Preheat the oven to 350°F (175°C).
@@ -19,16 +19,16 @@ cat >> recipes.txt <<- EOM
 EOM
 
 # Stage and commit the changes to recipes.txt on the feature branch
-git add recipes.txt
-git commit -m "Add chocolate cake recipe to recipes.txt"
+git add recipes.txt # <4>
+git commit -m "Add chocolate cake recipe to recipes.txt" 
 
 # Switch back to the default branch (main or master)
-git checkout main
+git checkout main # <5>
 
 # Make conflicting changes in the main branch
-echo "Vanilla Cake" >> recipes.txt
+echo "Vanilla Cake" >> recipes.txt # <6>
 cat >> recipes.txt <<- EOM
-
+# <9>
 1. Preheat the oven to 350°F (175°C).
 2. Mix flour, sugar, baking powder, and salt in a bowl.
 3. Add eggs, milk, oil, and vanilla extract, and mix well.
@@ -37,14 +37,15 @@ cat >> recipes.txt <<- EOM
 6. Let the cake cool before serving.
 EOM
 
-git add recipes.txt
-git commit -m "Add vanilla cake recipe to recipes.txt"
+git add recipes.txt # <7>
+git commit -m "Add vanilla cake recipe to recipes.txt" 
 
 # Attempt to merge the feature branch with the default branch to create a conflict
-git merge feature || true
+git merge feature || true # <8>
 
 # Resolve the merge conflict
-cat > recipes.txt <<- EOM
+cat > recipes.txt <<- EOM # <9>
+
 Chocolate Cake
 
 1. Preheat the oven to 350°F (175°C).
@@ -65,8 +66,8 @@ Vanilla Cake
 EOM
 
 # Stage and commit the resolved changes
-git add recipes.txt
-git commit -m "Resolve merge conflict and add both chocolate and vanilla cake recipes"
+git add recipes.txt # <10>
+git commit -m "Resolve merge conflict and add both chocolate and vanilla cake recipes" 
 
 # Delete the feature branch
-git branch -d feature
+git branch -d feature # <11>
