586ff4c7df21bca2eb1fe4249dc45216d9d80c70
konradpa <konrad@pagenstedt.de>
2024-07-22 12:03:04 +0200

    fix code-zip to handle multiple folders with different names

--- code/code-zip.sh
+++ code/code-zip.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-mkdir zips
+mkdir -p zips
 
-# First steps
+# Run the first script and zip its output
 sh code/code-exercises-first-steps-git.sh
 cp -r recipes recipes-first-steps
-zip -r zips/version-control-book-first-steps-git.zip recipes-first-steps
+zip -r zips/recipes-first-steps.zip recipes-first-steps
+rm -rf recipes recipes-first-steps
 
-# branches
+# Run the second script and zip its output
 sh code/code-exercises-branches.sh
 cp -r recipes recipes-branches
-zip -r zips/version-control-book-branches.zip recipes-branches
-
-# delete the recipe directories
-rm -rf recipes*
+zip -r zips/recipes-branches.zip recipes-branches
+rm -rf recipes recipes-branches
