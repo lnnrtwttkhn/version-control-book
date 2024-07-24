060a5c762916a8b487043edc28981c1392ec605c
Lennart Wittkuhn <lennart.wittkuhn@tutanota.com>
2024-07-24 14:50:24 +0200

    Don't remove recipes folder

--- code/code-zip.sh
+++ code/code-zip.sh
@@ -18,10 +18,10 @@ rm -rf recipes-setup
 sh code/code-exercises-first-steps-git.sh
 cp -r recipes recipes-first-steps
 zip -r zips/recipes-first-steps.zip recipes-first-steps
-rm -rf recipes recipes-first-steps
+rm -rf recipes-first-steps
 
 # Run the branches script and zip its output
 sh code/code-exercises-branches.sh
 cp -r recipes recipes-branches
 zip -r zips/recipes-branches.zip recipes-branches
-rm -rf recipes recipes-branches
+rm -rf recipes-branches
