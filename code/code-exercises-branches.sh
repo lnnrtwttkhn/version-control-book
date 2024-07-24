9c5adad6263e12b3589b8f24f682f6d79b8d7046
Lennart Wittkuhn <lennart.wittkuhn@tutanota.com>
2024-07-24 14:49:44 +0200

    Resolve merge conflict in branches exercise using sed

--- code/code-exercises-branches.sh
+++ code/code-exercises-branches.sh
@@ -7,15 +7,16 @@ cd recipes # <1>
 git checkout -b feature # <2>
 
 # Add a new recipe to your recipes.txt file
-echo "Chocolate Cake" >> recipes.txt # <3>
 cat >> recipes.txt <<- EOM
+
+Chocolate Cake
 
 1. Preheat the oven to 350°F (175°C).
 2. Mix flour, sugar, cocoa powder, baking powder, and salt in a bowl.
 3. Add eggs, milk, oil, and vanilla extract, and mix well.
 4. Pour the batter into a greased baking pan.
 5. Bake for 30-35 minutes.
-6. Let the cake cool before serving.
+
 EOM
 
 # Stage and commit the changes to recipes.txt on the feature branch
@@ -23,18 +24,19 @@ git add recipes.txt # <4>
 git commit -m "Add chocolate cake recipe to recipes.txt" 
 
 # Switch back to the default branch (main or master)
-git checkout main # <5>
+git switch main # <5>
 
 # Make conflicting changes in the main branch
-echo "Vanilla Cake" >> recipes.txt # <6>
 cat >> recipes.txt <<- EOM
-# <9>
+
+Vanilla Cake
+
 1. Preheat the oven to 350°F (175°C).
 2. Mix flour, sugar, baking powder, and salt in a bowl.
 3. Add eggs, milk, oil, and vanilla extract, and mix well.
-4. Pour the batter into a greased baking pan.
+4. Pour the batter into a greased baking pan. 
 5. Bake for 25-30 minutes.
-6. Let the cake cool before serving.
+
 EOM
 
 git add recipes.txt # <7>
@@ -44,30 +46,11 @@ git commit -m "Add vanilla cake recipe to recipes.txt"
 git merge feature || true # <8>
 
 # Resolve the merge conflict
-cat > recipes.txt <<- EOM # <9>
-
-Chocolate Cake
-
-1. Preheat the oven to 350°F (175°C).
-2. Mix flour, sugar, cocoa powder, baking powder, and salt in a bowl.
-3. Add eggs, milk, oil, and vanilla extract, and mix well.
-4. Pour the batter into a greased baking pan.
-5. Bake for 30-35 minutes.
-6. Let the cake cool before serving.
-
-Vanilla Cake
-
-1. Preheat the oven to 350°F (175°C).
-2. Mix flour, sugar, baking powder, and salt in a bowl.
-3. Add eggs, milk, oil, and vanilla extract, and mix well.
-4. Pour the batter into a greased baking pan.
-5. Bake for 25-30 minutes.
-6. Let the cake cool before serving.
-EOM
+sed -i '' -e '/^<<<<<<< /d' -e '/^=======/d' -e '/^>>>>>>> /d' recipes.txt
 
 # Stage and commit the resolved changes
 git add recipes.txt # <10>
-git commit -m "Resolve merge conflict and add both chocolate and vanilla cake recipes" 
+git commit -m "Resolve merge conflict by adding both chocolate and vanilla cake recipes" 
 
 # Delete the feature branch
 git branch -d feature # <11>
