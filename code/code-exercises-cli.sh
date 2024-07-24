3be22ac30cdacc432b9383b2020abeb5f214c9de
Lennart Wittkuhn <lennart.wittkuhn@tutanota.com>
2024-07-24 15:19:12 +0200

    Streamline CLI exercise and align the numbering of exercises

--- code/code-exercises-cli.sh
+++ code/code-exercises-cli.sh
@@ -1,23 +1,7 @@
 #!/bin/bash
 
-# Step 1: Identify a folder on your computer where you (want to) keep course-related files.
-# Tip: A good location might be within your "Documents" folder or a dedicated "Projects" or "Courses" folder. # 
-
-# Step 2: Navigate into the chosen location
-# Change the following path to your chosen directory
-# cd /path/to/your/chosen/directory # <1> 
-
-# Step 3: Use the command line to display the path of your chosen directory
-pwd # <2> 
-
-# Step 1: Within your chosen folder, create a new subfolder and name it `recipes`
-mkdir recipes # <3> 
-
-# Step 2: Navigate into the `recipes` folder
-cd recipes # <4> 
-
-# Step 3: Create a new file and name it `recipes.txt`
-touch recipes.txt # <5> 
-
-# Step 4: Confirm the creation of the file by listing the contents of the `recipes` folder
-ls # <6> 
+pwd # <2>
+mkdir recipes # <3>
+cd recipes # <4>
+touch recipes.txt # <5>
+ls # <6>
