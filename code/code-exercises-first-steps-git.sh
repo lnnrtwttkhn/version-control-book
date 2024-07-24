575c4544c52168facbea676475c19efbe3fda68d
Lennart Wittkuhn <lennart.wittkuhn@tutanota.com>
2024-07-24 19:06:27 +0200

    Update and align first steps with Git exercises

--- code/code-exercises-first-steps-git.sh
+++ code/code-exercises-first-steps-git.sh
@@ -1,8 +1,9 @@
-# Navigate to the existing 'recipes' directory
-cd recipes # <1>
+#!/bin/bash
 
-# Add content:
-cat > recipes.txt <<- EOM
+cd recipes # <1>
+git init # <2>
+touch recipes.txt # <3>
+cat > recipes.txt <<- EOM # <4>
 Pasta
 
 1. Bring water to the boil in a large saucepan.
@@ -11,15 +12,15 @@ Pasta
 4. Cook the pasta according to packet instructions until it's "al dente".
 5. Drain the pasta in a colander.
 EOM
-# <2>
-git add recipes.txt # <3>
-git commit -m "Add pasta recipe to recipes.txt" # <4>
-# Amend a commit:
-echo "6. Enjoy!" >> recipes.txt # <5>
-git add recipes.txt # <6>
-git commit --amend --no-edit # <7>
-# Create a .gitignore file:
-wget -O pasta.jpg https://upload.wikimedia.org/wikipedia/commons/8/84/Rigatoni.jpg # <8>
-echo "*.jpg" > .gitignore # <9>
-git add --all # <10>
-git commit -m "Add .gitignore and ignore all files that end with .jpg" # <11>
+git add recipes.txt # <5>
+git commit -m "Add pasta recipe to recipes.txt" # <6>
+echo "6. Enjoy!" >> recipes.txt # <7>
+git add recipes.txt # <8>
+git commit --amend --no-edit # <9>
+wget -O pasta.jpg https://upload.wikimedia.org/wikipedia/commons/8/84/Rigatoni.jpg # <10>
+git status # <11>
+touch .gitignore # <12>
+echo "*.jpg" > .gitignore # <13>
+git status # <14>
+git add --all # <15>
+git commit -m "Add .gitignore and ignore all files that end with .jpg" # <16>
