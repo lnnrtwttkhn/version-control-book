0ffb1649305fc225d81d38d85436569db3968937
Lennart Wittkuhn <lennart.wittkuhn@tutanota.com>
2024-07-25 21:26:42 +0200

    Save and display the output of the code execution

--- code/code-exercises.sh
+++ code/code-exercises.sh
@@ -3,17 +3,17 @@
 mkdir -p exercises-repos
 
 # Run the cli script and zip its output
-sh code/code-exercises-cli.sh
+sh -x code/code-exercises-cli.sh > "exercises-repos/history-cli.txt" 2>&1
 zip -r exercises-repos/recipes-cli.zip recipes
 
 # Run the setup script and zip its output
-sh code/code-exercises-setup-run.sh
+sh -x code/code-exercises-setup-run.sh > "exercises-repos/history-setup.txt" 2>&1
 zip -r exercises-repos/recipes-setup.zip recipes
 
 # Run the first steps script and zip its output
-sh code/code-exercises-first-steps-git.sh
+sh -x code/code-exercises-first-steps-git.sh > "exercises-repos/history-first-steps-git.txt" 2>&1
 zip -r exercises-repos/recipes-first-steps.zip recipes
 
 # Run the branches script and zip its output
-sh code/code-exercises-branches.sh
+sh -x code/code-exercises-branches.sh > "exercises-repos/history-branches.txt" 2>&1
 zip -r exercises-repos/recipes-branches.zip recipes
