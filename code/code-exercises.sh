f5176172bd3dc0caf734deca055eb4044ccf5cfa
Lennart Wittkuhn <lennart.wittkuhn@tutanota.com>
2024-07-25 21:11:05 +0200

    Rename make zips to make exercises

--- code/code-exercises.sh
+++ code/code-exercises.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-mkdir -p zips
+mkdir -p exercises-repos
 
 # Run the cli script and zip its output
 sh code/code-exercises-cli.sh
-zip -r zips/recipes-cli.zip recipes
+zip -r exercises-repos/recipes-cli.zip recipes
 
 # Run the setup script and zip its output
 sh code/code-exercises-setup-run.sh
-zip -r zips/recipes-setup.zip recipes
+zip -r exercises-repos/recipes-setup.zip recipes
 
 # Run the first steps script and zip its output
 sh code/code-exercises-first-steps-git.sh
-zip -r zips/recipes-first-steps.zip recipes
+zip -r exercises-repos/recipes-first-steps.zip recipes
 
 # Run the branches script and zip its output
 sh code/code-exercises-branches.sh
-zip -r zips/recipes-branches.zip recipes
+zip -r exercises-repos/recipes-branches.zip recipes
