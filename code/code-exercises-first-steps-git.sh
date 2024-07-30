5c3eeb4ce2494f1e6f6bb5754017116457d429d8
Lennart Wittkuhn <lennart.wittkuhn@tutanota.com>
2024-07-30 16:13:29 +0200

    Download smaller pasta image and make wget output --no-verbose

--- code/code-exercises-first-steps-git.sh
+++ code/code-exercises-first-steps-git.sh
@@ -17,7 +17,7 @@ git commit -m "Add pasta recipe to recipes.txt" # <6>
 echo "6. Enjoy!" >> recipes.txt # <7>
 git add recipes.txt # <8>
 git commit --amend --no-edit # <9>
-wget -O pasta.jpg https://upload.wikimedia.org/wikipedia/commons/8/84/Rigatoni.jpg # <10>
+wget -nv -O pasta.jpg https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Rigatoni.jpg/320px-Rigatoni.jpg # <10>
 git status # <11>
 touch .gitignore # <12>
 echo "*.jpg" > .gitignore # <13>
