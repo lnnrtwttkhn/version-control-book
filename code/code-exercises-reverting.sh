3192fedccc14047037635f29a10f881b6062602e
Lennart Wittkuhn <lennart.wittkuhn@tutanota.com>
2024-09-17 20:04:48 +0200

    Minor modifications in exercise on reverting

--- code/code-exercises-reverting.sh
+++ code/code-exercises-reverting.sh
@@ -5,6 +5,6 @@ git branch feature/revert-exercise # <2>
 git switch feature/revert-exercise # <3>
 echo "This is delicious!" >> recipes.txt # <4>
 git add recipes.txt # <5>
-git commit -m "add text" # <6>
+git commit -m "Add text" # <6>
 last_commit_hash=$(git log -1 --format="%H") # <7> 
 git revert --no-edit $last_commit_hash # <8> 
