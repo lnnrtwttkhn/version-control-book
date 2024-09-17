f8ebffe49921d875552d365e8b149ee7d114de6f
Lennart Wittkuhn <lennart.wittkuhn@tutanota.com>
2024-09-17 15:51:31 +0200

    Separate merge conflict exercise from branches exercise

--- code/code-exercises-branches.sh
+++ code/code-exercises-branches.sh
@@ -5,34 +5,20 @@ git branch feature # <2>
 git switch feature # <3>
 cat >> recipes.txt <<- EOM # <4>
 
-Chocolate Cake
+Potatoes
 
-1. Preheat the oven to 350°F (175°C).
-2. Mix flour, sugar, cocoa powder, baking powder, and salt in a bowl.
-3. Add eggs, milk, oil, and vanilla extract, and mix well.
-4. Pour the batter into a greased baking pan.
-5. Bake for 30-35 minutes.
-
+1. Wash the potatoes and cut them into evenly sized chunks.
+2. Bring water to the boil in a large saucepan.
+3. Once the water is boiling, add a good pinch of salt.
+3. Add the potatoes.
+4. Cook the potatoes for 15 - 20 minutes or until fork-tender.
+5. Drain the pasta in a colander.
 EOM
 git add recipes.txt # <5>
-git commit -m "Add chocolate cake recipe to recipes.txt" # <5>
-git checkout main # <6>
-cat >> recipes.txt <<- EOM # <7>
-
-Vanilla Cake
-
-1. Preheat the oven to 350°F (175°C).
-2. Mix flour, sugar, baking powder, and salt in a bowl.
-3. Add eggs, milk, oil, and vanilla extract, and mix well.
-4. Pour the batter into a greased baking pan. 
-5. Bake for 25-30 minutes.
-
-EOM
-
-git add recipes.txt # <8>
-git commit -m "Add vanilla cake recipe to recipes.txt" # <8>
+git commit -m "Add potatoes recipe to recipes.txt" # <5>
+cat recipes.txt # <6>
+git checkout main # <7>
+cat recipes.txt # <8>
 git merge feature # <9>
-sed -i '' -e '/^<<<<<<< /d' -e '/^=======/d' -e '/^>>>>>>> /d' recipes.txt # <10>
-git add recipes.txt # <11>
-git commit -m "Resolve merge conflict by adding both chocolate and vanilla cake recipes" # <12> 
-git branch -d feature # <13> 
+git branch -d feature # <10> 
+cat recipes.txt # <11>
