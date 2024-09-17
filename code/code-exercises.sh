96da24414dea710fb055eb9a9bc87aeff2bffd08
konradpa <135045175+konradpa@users.noreply.github.com>
2024-09-17 19:51:17 +0200

    Minor updates in branches chapter (#269)
    * answer question re:forking
    
    * add/change usecase for squashing
    
    * add disclaimer about previous commit to merge conflict section
    
    * add renaming branch master to main
    
    * add explanation about commit hash when deleting branch
    
    * add command for visualization of branches
    
    * add what happens to feature branch after merge
    
    * add section about deleting main branch
    
    * Update summary of branches chapter
    
    * Add a dot to the end of each branches objective
    
    * Review minor changes in branches chapter.
    
    * Add more commands to branches cheatsheet and edit a few others
    
    * Separate merge conflict exercise from branches exercise
    
    * Add separate section for merge conflict exercise
    
    * Fix path to zip file after merge conflict exercise
    
    * Insert question about forking into callout block
    
    * Fix broken links to remotes-intro chapter
    
    ---------
    
    Co-authored-by: Lennart Wittkuhn <lennart.wittkuhn@tutanota.com>

--- code/code-exercises.sh
+++ code/code-exercises.sh
@@ -17,3 +17,7 @@ zip -r exercises-repos/recipes-first-steps.zip recipes
 # Run the branches script and zip its output
 sh -x code/code-exercises-branches.sh > "exercises-repos/history-branches.txt" 2>&1
 zip -r exercises-repos/recipes-branches.zip recipes
+
+# Run the branches merge conflict script and zip its output
+sh -x code/code-exercises-branches-merge-conflict.sh > "exercises-repos/history-branches-merge-conflict.txt" 2>&1
+zip -r exercises-repos/recipes-branches-merge-conflict.zip recipes
