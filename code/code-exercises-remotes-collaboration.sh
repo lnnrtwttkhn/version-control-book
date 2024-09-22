a8ddf6aba61b660cffb82d2e09898fe32621cc18
Lennart Wittkuhn <lennart.wittkuhn@tutanota.com>
2024-09-22 17:41:43 +0200

    Fix numbering in collaboration exercise

--- code/code-exercises-remotes-collaboration.sh
+++ code/code-exercises-remotes-collaboration.sh
@@ -8,5 +8,6 @@ echo "New Recipe" >> recipes.txt # <5>
 git add recipes.txt  # <6>
 git commit -m "Add new recipe to recipes.txt" # <6>
 git push origin new-branch-name # <7>
-# Review the PR your partner made in your repository. # <8>
-# Merge the PR into your repository. # <9>
+# Create a Pull / Merge Request. # <8>
+# Review the PR your partner made in your repository. # <9>
+# Merge the PR into your repository. # <10>
