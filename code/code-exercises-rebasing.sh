17ec7bcc8a64e29640e486a79c077dbfb6b049ec
Lennart Wittkuhn <lennart.wittkuhn@tutanota.com>
2024-09-22 14:54:10 +0200

    Update rebasing exercise

--- code/code-exercises-rebasing.sh
+++ code/code-exercises-rebasing.sh
@@ -5,12 +5,12 @@ git branch feature/rebase-exercise # <2>
 git switch feature/rebase-exercise # <3>
 echo "New feature content" >> feature.txt # <4>
 git add feature.txt # <5>
-git commit -m "Added feature content" # <6>
+git commit -m "Add feature content" # <6>
 git switch main # <7>
 echo "Main branch content" >> main.txt # <8>
 git add main.txt # <9>
-git commit -m "Updated main content" # <10>
+git commit -m "Update main content" # <10>
 git switch feature/rebase-exercise # <11>
 git rebase main  # <12>
 git switch main # <13>
-git log # <14>
+git log --oneline # <14>
