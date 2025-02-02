d4e2f308373b96c539b73173cc93ca81d74fedd6
Lennart Wittkuhn <42233065+lnnrtwttkhn@users.noreply.github.com>
2025-02-02 01:39:12 +0100

    Delete code/code-exercises-remotes-push-city-guide.sh

--- code/code-exercises-remotes-push-city-guide.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# create an empty remote repository # <1>
-cd city-guide # <2>
-git remote add origin https://github.com/your-username/your-repo-name.git # <3>
-git push -u origin main # <4>
